9d909104f58287cd1b5d59a6c045ce957495c27a
Dhineshkumaru <dhineshkumarugendran@gmail.com>
2021-02-24 15:08:23 +0530

    2 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -4,13 +4,29 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
   selector: "hello",
   template: `
     <h1>
-      Hello {{ name }}! <button (click)="SenddataToParent()">click</button>
+      Hello {{ name }}!
+      <button class="btn-style" (click)="SenddataToParent()">Click</button>
     </h1>
   `,
   styles: [
     `
       h1 {
         font-family: Lato;
+      }
+      button:hover {
+        background-color: yellow;
+      }
+      .btn-style {
+        color: white;
+        width: 200px;
+        height: 30px;
+        font-size: 15px;
+        font-weight: bold;
+        cursor: pointer;
+        background-color: yellowgreen;
+        border: 2px solid black;
+        border-radius: 20px;
+        hover: black;
       }
     `
   ]
